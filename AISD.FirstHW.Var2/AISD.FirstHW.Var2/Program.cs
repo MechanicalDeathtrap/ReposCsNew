cfb37279bbc0f4c1c494f6c269b5beda82422368
MechanicalDeathtrap <korikova-04@mail.ru>
2023-04-10 21:00:37 +0300

    lists

---
+++ AISD.FirstHW.Var2/AISD.FirstHW.Var2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AISD.FirstHW.Var2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyLinkedList<int> list = new MyLinkedList<int>();
+            var array1 = new int[] {1,2,3,4 };
+            var array2 = new int[] {11,12,5,6,7,8,9};
+            var array3 = new int[] {16,34,45,66,77,88,10};
+            list.Add(array1);
+            list.Add(array2);
+            list.Add(array3);
+            list.Get(4);
+            list.Get(5);
+            list.Get(11);
+            list.Get(12);
+            list.Get(13);
+        }
+    }
+}
---
+++ AISD.FirstHW.Var2/AISD.FirstHW.Var2/QueueItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AISD.FirstHW.Var2
+{
+    internal class QueueItem<T> where T : IComparable<T>
+    {
+        public T[] value { get; set; }
+        public QueueItem<T> nextArray { get; set; }
+
+        public QueueItem(T[] array)
+        {
+            value=array;
+            nextArray = null;
+
+        }
+    }
+}
